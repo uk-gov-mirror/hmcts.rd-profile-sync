0bc88f8888595be79bb3af9c29147f75734ef891
ConorH994 <53298170+ConorH994@users.noreply.github.com>
2020-02-18 16:49:48 +0000

    Nightly build fix (#159)
    * change to kick off jenkins
    
    * changes
    
    * suppressing hmtl unit
    
    * upgrade spring
    
    * removed   aadIdentityName value from values file
    
    * reverted spring version as its failing build
    
    * upgrades
    
    * change
    
    * no choice but to suppress, upgrade doesnt work, fails build
    
    * removed enableDockBuild
    
    * no choice but to ignore
    
    * Removed whitespace
    
    * reverted change
    
    * update spring
    
    * in latest version of spring, APPLICATION_JSON_UTF8_VALUE is deprecated in favour of APPLICATION_JSON_VALUE
    
    * added require suppresions
    
    * upgraded selenium opera
    
    * selenium throws false positives even on latest version, suppressing
    
    * removed unnecessary suppressions

--- src/main/java/uk/gov/hmcts/reform/profilesync/client/UserProfileClient.java
+++ src/main/java/uk/gov/hmcts/reform/profilesync/client/UserProfileClient.java
@@ -20,20 +20,20 @@ import uk.gov.hmcts.reform.profilesync.domain.UserProfile;
 @FeignClient(name = "userProfileClient", url = "${userprofile.api.url}")
 public interface UserProfileClient {
 
-    @PutMapping(value = "/v1/userprofile/{userId}", consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE}, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @PutMapping(value = "/v1/userprofile/{userId}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     @Headers({"authorization: {authorization}", "serviceauthorization: {serviceauthorization}"})
     public Response syncUserStatus(@RequestHeader("authorization") String authorization,
                                    @RequestHeader("serviceauthorization") String serviceAuthorization,
                                    @PathVariable("userId") String userId, @RequestBody UserProfile body);
 
 
-    @GetMapping(value = "/v1/userprofile", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @GetMapping(value = "/v1/userprofile", produces = {MediaType.APPLICATION_JSON_VALUE})
     @Headers({"authorization: {authorization}", "serviceauthorization: {serviceauthorization}"})
     public Response findUser(@RequestHeader("authorization") String authorization,
                                    @RequestHeader("serviceauthorization") String serviceAuthorization,
                                    @RequestParam("userId") String userId);
 
-    @PostMapping(value = "/api/v1/users/registration", consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    @PostMapping(value = "/api/v1/users/registration", consumes = {MediaType.APPLICATION_JSON_VALUE})
     @Headers("ServiceAuthorization: {ServiceAuthorization}")
     public Response createUserProfile(@Valid @RequestBody Object createUserProfileData);
 }
