a3ee07d603aff10601d8582196d16f84aaedebff
Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>
2020-03-19 12:52:02 +0000

    RDCC-863: moving changes from Master to Demo (#193)
    * RDCC-863: moving changes from Master to Demo
    
    * RDCC-863: amending build.gradle
    
    * RDCC-863: amending build.gradle

--- src/integrationTest/java/uk/gov/hmcts/reform/profilesync/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/profilesync/SpringBootIntegrationTest.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.reform.profilesync;
 
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 @SpringBootTest(classes = ProfileSyncApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class SpringBootIntegrationTest {
 
