585ea18032c2a32cab8e1eaa636a5d9991319f7d
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2020-08-28 17:57:06 +0100

    Removed unnecessary TF resources

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,12 +1,12 @@
 
-output "resourceGroup" {
-  value = "${azurerm_resource_group.rg.name}"
-}
+# output "resourceGroup" {
+#   value = "${azurerm_resource_group.rg.name}"
+# }
 
-output "appServicePlan" {
-  value = "${local.app_service_plan}"
-}
+# output "appServicePlan" {
+#   value = "${local.app_service_plan}"
+# }
 
-output "vaultUri" {
-  value = "${local.s2s_vault_uri}"
-}
+# output "vaultUri" {
+#   value = "${local.s2s_vault_uri}"
+# }
