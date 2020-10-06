0abdedb1750732ec3199b0d046f7e0aa565f10f2
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2020-10-06 11:35:01 +0100

    Upgrade TF to 0.12.2 and Azure RM to 2.20.0 (#381)

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -2,11 +2,3 @@
 output "resourceGroup" {
   value = "${azurerm_resource_group.rg.name}"
 }
-
-output "appServicePlan" {
-  value = "${local.app_service_plan}"
-}
-
-output "vaultUri" {
-  value = "${local.s2s_vault_uri}"
-}
